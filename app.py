f9cff918bf864ded8fceba0898a42c5611026314
Dhanapalnv <76559967+Dhanapalnv@users.noreply.github.com>
2025-02-21 12:25:37 +0530

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ import streamlit.components.v1 as components
 # Set up Streamlit page configuration
 st.set_page_config(
     page_title="Mr.Paul's Favicon Finder", 
-    page_icon="🦋🌀", 
+    page_icon="🦋", 
     layout="wide"
 )
 
